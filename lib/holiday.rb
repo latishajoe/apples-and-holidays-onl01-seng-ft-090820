3c54a1e750e4bf5538f4cc8abd29c2a8f42e1447
Latisha Joe <latisha_joe@yahoo.com>
2020-09-19 08:15:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,9 +65,9 @@ def all_supplies_in_holidays(holiday_hash)
   holiday.each do |holiday, supplies|
     supplies= supplies.join(", ")
     holiday = holiday.to_s.split("_").map {|w| w.capitalize}.join(" ")<< ":"
-    puts " " + holiday + " " + supplies 
-  end 
- end 
+    puts " " + holiday + " " + supplies
+  end
+ end
 end
 
 def all_holidays_with_bbq(holiday_hash)
